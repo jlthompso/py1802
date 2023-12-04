fc9717100c816a4d244ac84bc74920d1588f9fc6
Joe Thompson <jthompson7@blueorigin.com>
2023-12-04 15:00:34 -0800

    Define rest of register ops.

--- instructions.py
+++ instructions.py
@@ -7,22 +7,59 @@ if TYPE_CHECKING:
 
 def decode(cpu: CPU, I: int, N: int):
     match I, N:
-        case 0x0, 0x0:
-            IDL(cpu)
-        case 0x0, N:
-            LDN(cpu, N)
-        case 0x1, N:
-            INC(cpu, N)
-        case 0xC, 0x4:
-            NOP(cpu)
+        case 0x0, 0x0: IDL(cpu)
+        case 0x0, N: LDN(cpu, N)
+        case 0x1, N: INC(cpu, N)
+        case 0x2, N: DEC(cpu, N)
+        case 0x6, 0x0: IRX(cpu)
+        case 0x8, N: GLO(cpu, N)
+        case 0x9, N: GHI(cpu, N)
+        case 0xA, N: PLO(cpu, N)
+        case 0xB, N: PHI(cpu, N)
+        case 0xC, 0x4: NOP(cpu)
         case _, _:
             raise NotImplementedError("Attempted to execute invalid instruction.")
+
+
+def DEC(cpu: CPU, N: int):
+    # Decrement Register N (0x2N)
+    #   R(N)-1
+    cpu.R[N] = cpu.R[N] - 1 if cpu.R[N] - 1 >= 0 else 0xFFFF
+    cpu.set_state(states.Fetch())
 
 
 def IDL(cpu: CPU):
     # Idle (0x00):
     #   M(R(0))-->BUS
     cpu.BUS = cpu.M[cpu.R[0]]
+
+
+def INC(cpu: CPU, N: int):
+    # Increment Register N (0x1N):
+    #   R(N)+1
+    cpu.R[N] = (cpu.R[N] + 1) % 0xFFFF
+    cpu.set_state(states.Fetch())
+
+
+def IRX(cpu: CPU):
+    # Increment Register X (0x60)
+    #   R(X)+1
+    cpu.R[cpu.X] = (cpu.R[cpu.X] + 1) % 0xFFFF
+    cpu.set_state(states.Fetch())
+
+
+def GHI(cpu: CPU, N: int):
+    # Get High Register N
+    #   R(N).1-->D
+    cpu.D = (cpu.R[N] >> 8) & 0xFF
+    cpu.set_state(states.Fetch())
+
+
+def GLO(cpu: CPU, N: int):
+    # Get Low Register N
+    #   R(N).0-->D
+    cpu.D = cpu.R[N] & 0xFF
+    cpu.set_state(states.Fetch())
 
 
 def LDN(cpu: CPU, N: int):
@@ -32,10 +69,17 @@ def LDN(cpu: CPU, N: int):
     cpu.set_state(states.Fetch())
 
 
-def INC(cpu: CPU, N: int):
-    # Increment Register N (0x1N):
-    #   R(N)+1
-    cpu.R[cpu.N] = (cpu.R[N] + 1) % 0xFFFF
+def PHI(cpu: CPU, N: int):
+    # Put High Register N
+    #   D-->R(N).1
+    cpu.R[N] &= ((cpu.D << 8) & 0xFF00) | 0x00FF
+    cpu.set_state(states.Fetch())
+
+
+def PLO(cpu: CPU, N: int):
+    # Put Low Register N
+    #   D-->R(N).0
+    cpu.R[N] &= 0xFF00 + cpu.D
     cpu.set_state(states.Fetch())
 
 
